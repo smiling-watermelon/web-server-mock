93c21ab048d9163c05347b328c3b7aae84ad07f3
smiling-watermelon <146741211+smiling-watermelon@users.noreply.github.com>
2026-05-22 08:56:01 +0000

    add ruff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
-import fastapi
 import random
 import time
+
+import fastapi
 
 app = fastapi.FastAPI()
 
@@ -15,10 +16,17 @@ handler.setFormatter(formatter)
 main_logger.addHandler(handler)
 
 
-def make_log(logger: "logging.Logger", data: dict, level: str = "info"):
+def make_log(
+    logger: logging.Logger,
+    data: dict,
+    level: str = "info",
+) -> None:
     log_method = getattr(logger, level, logger.info)
     log_method(data)
 
+
+TIMEOUT_CHANCE = 0.25
+DELAY_CHANCE = 0.5
 
 CORS_HEADERS = {
     "Access-Control-Allow-Headers": "*",
@@ -35,7 +43,7 @@ RESPONSE = {
 def get_order(response: fastapi.Response) -> fastapi.Response:
     random_value = random.random()
 
-    if random_value < 0.25:
+    if random_value < TIMEOUT_CHANCE:
         make_log(
             main_logger,
             {
@@ -48,7 +56,7 @@ def get_order(response: fastapi.Response) -> fastapi.Response:
             status_code=529,
             content="<html><body><h1>Server is busy. Please try again later.</h1></body></html>",
         )
-    if random_value < 0.5:
+    if random_value < TIMEOUT_CHANCE + DELAY_CHANCE:
         sleep_time = random.randint(5, 30)
         make_log(
             main_logger,
@@ -75,7 +83,9 @@ def get_order(response: fastapi.Response) -> fastapi.Response:
 
 
 @app.options("/endpoint")
-def options_order_by_id(response: fastapi.Response) -> fastapi.Response:
+def options_order_by_id(
+    response: fastapi.Response,  # noqa: ARG001
+) -> fastapi.Response:
     return fastapi.Response(
         status_code=204,
         headers=CORS_HEADERS.copy(),
